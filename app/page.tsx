61b7859053bb543805137218265868a65dcca253
Beseder00 <substacklists@gmail.com>
2025-03-22 13:24:13 +0200

    Add Next.js app directory structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { PreLaunchLanding } from "@/components/pre-launch-landing";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <PreLaunchLanding />
+    </main>
+  );
+}
